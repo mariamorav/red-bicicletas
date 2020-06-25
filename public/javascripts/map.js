3a2118bdf3ad9220247b016dd304a4b7efd88824
Maria Mora <mariamora2807@gmail.com>
2020-06-24 19:55:50 -0500

    Practica MVC

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -3,3 +3,7 @@ var map = L.map('main_map').setView([7.898543, -72.495908], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+L.marker([7.898543, -72.495908]).addTo(map)
+    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+    .openPopup();
