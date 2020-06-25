c478676e4327cc431df18aa989c2489b0c26f9eb
Maria Mora <mariamora2807@gmail.com>
2020-06-24 21:20:03 -0500

    Se agregaron carpetas api y se crearon las rutas de actualizacion, listado, eliminación e inserción

--- public/javascripts/map.js
+++ public/javascripts/map.js
@@ -4,6 +4,17 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-L.marker([7.898543, -72.495908]).addTo(map)
-    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
-    .openPopup();
+L.marker([7.898543, -72.495908]).addTo(map);
+L.marker([7.898892, -71.499328]).addTo(map);
+L.marker([8.898543, -71.499328]).addTo(map);
+
+$.ajax({
+    dataType: "json",
+    url: "api/bicicletas",
+    success: function(result){
+        console.log(result);
+        result.bicicletas.forEach(function(bici){
+            L.marker(bici.ubicacion, {title: bici.id}).addTo(map);
+        });
+    }
+});
