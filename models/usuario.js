137943dd92700441ed082385319ca20fe50b167f
Maria Mora <mariamora2807@gmail.com>
2020-08-17 19:57:31 -0500

    modificaciones al modelo usuario

--- models/usuario.js
+++ models/usuario.js
@@ -1,10 +1,58 @@
-var mongoose = require('mongoose');
-var Reserva = require('./reserva');
-var Schema = mongoose.Schema;
+const mongoose = require('mongoose');
+const Reserva = require('./reserva');
+const uniqueValidator = require ('mongoose-unique-validator');
 
-var usuarioSchema = new Schema ({
-    nombre: String,
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+const Schema = mongoose.Schema;
+
+const validateEmail = function(email) {
+    const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/
+    return re.test(email);
+};
+
+const usuarioSchema = new Schema ({
+    nombre: {
+        type: String,
+        trim: true,
+        required: [true, 'El nombre es obligatorio']
+    },
+    email: {
+        type: String,
+        trim: true,
+        required: [true, 'El email es obligatorio'],
+        lowercase: true,
+        unique: true,
+        validate: [validateEmail, 'Por favor, ingrese un email valido'],
+        match: [/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/]
+    },
+    password: {
+        type: String,
+        required: [true, 'El password es obligatorio']
+    },
+    passwordResetToken: String,
+    passwordResetTokenExpires: Date,
+    verificado: {
+        type: Boolean,
+        default: false
+    }
+    
+
 });
+
+usuarioSchema.plugin(uniqueValidator, {message: 'El {PATH} ya existe con otro usuario'});
+
+usuarioSchema.pre('save', function(next){
+    if(this.isModified('password')){
+        this.password = bcrypt.hashSync(this.password, saltRounds);
+    }
+    next();
+})
+
+usuarioSchema.methods.validPassword = function(password) {
+    return bcrypt.compareSync(password, this.password);
+};
 
 usuarioSchema.methods.reservar = function(biciId, desde, hasta, cb){
     var reserva = new Reserva ({usuario: this._id, bicicleta: biciId, desde:desde, hasta:hasta});
