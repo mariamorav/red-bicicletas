433c830084006a50698736dcd57edf93e2e9e07c
Maria Mora <mariamora2807@gmail.com>
2020-10-07 21:32:27 -0500

    (improve) implement sendgrid in mailer config

--- mailer/mailer.js
+++ mailer/mailer.js
@@ -1,12 +1,45 @@
+const { log } = console;
 const nodemailer = require('nodemailer');
+const sg_transport = require('nodemailer-sendgrid-transport');
 
-const mailConfig = {
-    host: 'smtp.ethereal.email',
-    port: 587,
-    auth: {
-        user: 'arvid82@ethereal.email',
-        pass: 'mSbe1BZHGu2TG8wS86'
+let mailConfig;
+
+if(process.env.NODE_ENV === 'production'){
+
+    const options = {
+        auth: {
+            api_key: process.env.SENDGRID_API_SECRET_KEY
+        }
     }
-};
+    
+    mailConfig = sgTransport(options);
+
+} else {
+
+    if(process.env.NODE_ENV === 'staging') {
+
+        log('::::: STAGING ::::::');
+        const options = {
+            auth: {
+                api_key: process.env.SENDGRID_API_SECRET_KEY
+            }
+        }
+
+        mailConfig = sg_transport(options);
+
+    } else {
+
+        mailConfig = {
+            host: 'smtp.ethereal.email',
+            port: 587,
+            auth: {
+                user: process.env.ETHEREAL_USER,
+                pass: process.env.ETHEREAL_PASS
+            }
+        };
+        
+    }
+}
+
 
 module.exports = nodemailer.createTransport(mailConfig);
